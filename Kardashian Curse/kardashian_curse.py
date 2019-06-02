84f6f4ce1889eb60b1368515031e53e05efccd51
Andrew Cho <andycho7@gmail.com>
2019-06-01 20:59:50 -0400

    single chart

--- Kardashian Curse/kardashian_curse.py
+++ Kardashian Curse/kardashian_curse.py
@@ -10,15 +10,13 @@ from datetime import datetime
 #Kendall Jenner, Blake Griffin; August 2017 - February 2018; 
 #Tristan Thompson, Khloe Kardashian; July 2016 - February 19, 2019
 #Ben Simmons, Kendall Jenner; June 12, 2018 - May 22, 2019
-#Lamar Odom
-#Kris Humphries
-#Jordan Clarkson
-#James Harden
+#Lamar Odom, Khloe Kardashian; August 2009 - December 13, 2013
+#Kris Humphries; November 2010 - November 2011
 #Game Score, PER
 
 
 #%%
-player_keys = ['griffbl01', 'thomptr01', 'simmobe01']
+player_keys = ['griffbl01', 'thomptr01', 'simmobe01', 'odomla01', 'humphkr01']
 
 #pull headers
 url = 'https://www.basketball-reference.com/players/g/griffbl01/gamelog/2017'
@@ -31,7 +29,7 @@ headers = headers[1:]
 
 #pull individual seasons for player
 def pull_gamelogs(player_key):
-    year = 2010
+    year = 1999 #first year of Lamar Odom's career (oldest player)
     complete_stats = pd.DataFrame()
     while (year <= 2019):
         #URL to scrape
@@ -58,11 +56,17 @@ tt_stats = pull_gamelogs(player_keys[1])
 tt_stats['Player'] = 'Tristan Thompson'
 bs_stats = pull_gamelogs(player_keys[2])
 bs_stats['Player'] = 'Ben Simmons'
+lo_stats = pull_gamelogs(player_keys[3])
+lo_stats['Player'] = 'Lamar Odom'
+kh_stats = pull_gamelogs(player_keys[4])
+kh_stats['Player'] = 'Kris Humphries'
 
 #%%
 bg_stats['Date'] = pd.to_datetime(bg_stats['Date'])
 tt_stats['Date'] = pd.to_datetime(tt_stats['Date'])
 bs_stats['Date'] = pd.to_datetime(bs_stats['Date'])
+lo_stats['Date'] = pd.to_datetime(lo_stats['Date'])
+kh_stats['Date'] = pd.to_datetime(kh_stats['Date'])
 
 #%%
 bg_stats.index = bg_stats['Date']
@@ -71,44 +75,44 @@ tt_stats.index = tt_stats['Date']
 del tt_stats['Date']
 bs_stats.index = bs_stats['Date']
 del bs_stats['Date']
+lo_stats.index = lo_stats['Date']
+del lo_stats['Date']
+kh_stats.index = kh_stats['Date']
+del kh_stats['Date']
 
 #%%
 bg_stats['GmSc'] = bg_stats['GmSc'].astype('float64')
 tt_stats['GmSc'] = tt_stats['GmSc'].astype('float64')
 bs_stats['GmSc'] = bs_stats['GmSc'].astype('float64')
+lo_stats['GmSc'] = lo_stats['GmSc'].astype('float64')
+kh_stats['GmSc'] = kh_stats['GmSc'].astype('float64')
 
 
 #%%
 bg_stats_weekly = bg_stats.resample('W').mean()
-bg_stats_weekly['rolling_GmSc'] = bg_stats_weekly['GmSc'].rolling( window=3).mean()
+bg_stats_weekly['rolling_GmSc'] = bg_stats_weekly['GmSc'].rolling( window=2).mean()
 tt_stats_weekly = tt_stats.resample('W').mean()
-tt_stats_weekly['rolling_GmSc'] = tt_stats_weekly['GmSc'].rolling( window=3).mean()
+tt_stats_weekly['rolling_GmSc'] = tt_stats_weekly['GmSc'].rolling( window=2).mean()
 bs_stats_weekly = bs_stats.resample('W').mean()
-bs_stats_weekly['rolling_GmSc'] = bs_stats_weekly['GmSc'].rolling( window=3).mean()
+bs_stats_weekly['rolling_GmSc'] = bs_stats_weekly['GmSc'].rolling( window=2).mean()
+lo_stats_weekly = lo_stats.resample('W').mean()
+lo_stats_weekly['rolling_GmSc'] = lo_stats_weekly['GmSc'].rolling( window=2).mean()
+kh_stats_weekly = kh_stats.resample('W').mean()
+kh_stats_weekly['rolling_GmSc'] = kh_stats_weekly['GmSc'].rolling( window=2).mean()
 
 #%%
 trace1 = go.Scatter(
     x = bg_stats_weekly.index,
-    y = bg_stats_weekly['GmSc'],
+    y = bg_stats_weekly['rolling_GmSc'],
     name='Blake Griffin'
-)
-trace2 = go.Scatter(
-    x = tt_stats_weekly.index,
-    y = tt_stats_weekly['GmSc'],
-    name='Tristan Thompson'
-)
-trace3 = go.Scatter(
-    x = bs_stats_weekly.index,
-    y = bs_stats_weekly['GmSc'],
-    name='Ben Simmons'
 )
 
 layout = go.Layout(
-    title="Game Scores", 
+    title="Blake Griffin 2 Week Roling Game Scores", 
     xaxis={'title':'Date'}, 
     yaxis={'title':'Weekly Mean Game Score'},
     shapes=[
-        # Blake & Kendall
+        # Kendall
         {
             'type': 'line',
             'x0': datetime.strptime('2017-08-01', '%Y-%m-%d'),
@@ -116,10 +120,11 @@ layout = go.Layout(
             'x1': datetime.strptime('2017-08-02', '%Y-%m-%d'),
             'y1': 30,
             'line': {
-                'color': 'rgb(55, 128, 191)',
+                'color': 'red',
                 'width': 3,
                 'dash': 'dashdot'
             },
+            'layer': 'below'
         },
         {
             'type': 'line',
@@ -128,61 +133,25 @@ layout = go.Layout(
             'x1': datetime.strptime('2018-02-02', '%Y-%m-%d'),
             'y1': 30,
             'line': {
-                'color': 'rgb(55, 128, 191)',
+                'color': 'red',
                 'width': 3,
                 'dash': 'dashdot'
             },
-        },
-        # Tristan & Khloe
-        {
-            'type': 'line',
-            'x0': datetime.strptime('2016-07-01', '%Y-%m-%d'),
-            'y0': 0,
-            'x1': datetime.strptime('2016-07-02', '%Y-%m-%d'),
-            'y1': 30,
-            'line': {
-                'color': 'orange',
-                'width': 3,
-                'dash': 'dashdot'
-            },
-        },
-        {
-            'type': 'line',
-            'x0': datetime.strptime('2019-02-19', '%Y-%m-%d'),
-            'y0': 0,
-            'x1': datetime.strptime('2019-02-20', '%Y-%m-%d'),
-            'y1': 30,
-            'line': {
-                'color': 'orange',
-                'width': 3,
-                'dash': 'dashdot'
-            },
-        },
-        # Ben & Kendall
-        {
-            'type': 'line',
-            'x0': datetime.strptime('2018-06-12', '%Y-%m-%d'),
-            'y0': 0,
-            'x1': datetime.strptime('2018-06-13', '%Y-%m-%d'),
-            'y1': 30,
-            'line': {
-                'color': 'green',
-                'width': 3,
-                'dash': 'dashdot'
-            },
-        },
-        {
-            'type': 'line',
-            'x0': datetime.strptime('2019-05-22', '%Y-%m-%d'),
-            'y0': 0,
-            'x1': datetime.strptime('2019-05-23', '%Y-%m-%d'),
-            'y1': 30,
-            'line': {
-                'color': 'green',
-                'width': 3,
-                'dash': 'dashdot'
-            },
-        },
+            'layer': 'below'
+        }
+    ],
+    annotations=[
+        dict(
+            x=datetime.strptime('2017-11-01', '%Y-%m-%d'),
+            y=32,
+            xref='x',
+            yref='y',
+            text='Kendall Jenner',
+            showarrow=False,
+            arrowhead=7,
+            ax=0,
+            ay=-40
+        )
     ],
     autosize=False,
     width=600,
@@ -195,7 +164,7 @@ layout = go.Layout(
         pad=4
     )
 )
-fig = go.Figure(data=[trace1,trace2,trace3], layout=layout)
+fig = go.Figure(data=[trace1], layout=layout)
 iplot(fig, filename='time-series-simple')
 
 #%%
