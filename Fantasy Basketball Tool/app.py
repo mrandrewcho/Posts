a9095e7bcc158914171d2c9a48691a1735b61d71
Andrew Cho <andycho7@gmail.com>
2019-11-14 21:07:33 -0500

    messing around w/ teams

--- Fantasy Basketball Tool/app.py
+++ Fantasy Basketball Tool/app.py
@@ -1,43 +1,63 @@
-import urllib2
+#%%
 from bs4 import BeautifulSoup
+import requests
 import pandas as pd
 
+#%%
 url = 'https://basketballmonster.com/playerrankings.aspx'
 
-html = urllib2.urlopen(url).read()
-soup = BeautifulSoup(html,'lxml')
-
-soup.findAll('div', class_='results-table')[0].findAll('th', limit=30)
-
-column_headers = [th.getText() for th in soup.findAll('div', class_='results-table')[0].findAll('th', limit=30)]
-
-column_headers = [x.encode('utf-8') for x in column_headers]
-
-data_rows = soup.findAll('div', class_='results-table')[0].findAll('td')
-
-#get the ones where class is not empty
-
-player_data =[]
-columns = ['Round', 'Rank', 'Value', 'Name', 'Inj', 'Team', 'Pos', 'g', 'm/g', 'p/g', '3/g', 'r/g', 'a/g', 's/g', 'b/g', 'fg%', 'fga/g', 'ft%', 'fta/g', 'to/g', 'pV', '3V', 'rV', 'aV', 'sV', 'bV', 'fg%V', 'ft%V', 'toV']
-
-player_data = pd.DataFrame(columns=columns)
-
-for i in range(0, len(data_rows)):
-    if i == 0:
-        
-        print(data_rows[i])
-
-print(player_data)
+html = requests.get(url).text
+soup = BeautifulSoup(html,'html.parser')
 
 
-while(attribute < 30):
-    player_row = []
-    for i in range(0,10):#len(data_rows)):
-        print(data_rows[i])
-        #if (data_rows[i] != data_rows[0]):
-            #player_row = player_row.append(data_rows[i])
-    player_data.append(player_row)
-    attribute+=1
 
-#for row in data_rows:
-#    print(row)
+#%%
+#cats are in table headers (MEOWW)
+#cells are in td
+
+#%%
+#get table headers and put into a list
+results_table = soup.find('div', {'class': 'results-table'})
+results_table_header = results_table.find('thead')
+table_headers = results_table_header.find_all('th')
+
+column_headers =[]
+
+for item in table_headers:
+    column_headers.append(item.text)
+
+column_headers
+
+#%%
+#get individual player's stats
+results_table_rows = results_table.find_all('tr')  
+
+player_db = pd.DataFrame(columns = column_headers)
+
+for rows in results_table_rows:
+    player_data = []
+    player_cells = rows.find_all('td')
+    for cell in player_cells:
+        player_data.append(cell.text)
+    if len(player_data) > 0:
+        player_db = player_db.append(pd.Series(player_data, index=column_headers), ignore_index=True) #need to convert to series for column names
+
+#%%
+player_db
+
+#%%
+#change dtypes
+player_db[['Round', 'Rank', 'Value', 'g', 'm/g', 'p/g', '3/g', 'r/g', 'a/g', 's/g', 'b/g', 'fg%', 'fga/g', 'ft%', 'fta/g', 'to/g', 'pV', '3V', 'rV', 'aV', 'sV', 'bV', 'fg%V', 'ft%V', 'toV']] = player_db[['Round', 'Rank', 'Value', 'g', 'm/g', 'p/g', '3/g', 'r/g', 'a/g', 's/g', 'b/g', 'fg%', 'fga/g', 'ft%', 'fta/g', 'to/g', 'pV', '3V', 'rV', 'aV', 'sV', 'bV', 'fg%V', 'ft%V', 'toV']].apply(pd.to_numeric)
+
+#%%
+my_team = ['Shai Gilgeous-Alexander', 'C.J. McCollum', 'Otto Porter', 'Montrezl Harrell', 'John Collins', 'Ben Simmons', 'Kristaps Porzingis', "D'Angelo Russell", 'Mitchell Robinson', 'Jason Richardson', 'Myles Turner', 'Cody Zeller', 'P.J. Tucker']
+
+my_players = player_db[player_db['Name'].isin(my_team)]
+
+#%%
+my_players.describe()
+
+#%%
+
+
+#%%
